6cc6c42970be08d7f881609cddac801e5fa17d50
Gustavo Guimarães <gustavo@Gustavos-Mac-mini.local>
2025-06-16 15:01:42 -0300

    Adiciona .gitignore e remove arquivos desnecessários

--- Pods/Target Support Files/Pods-Moises-MoisesUITests/Pods-Moises-MoisesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Moises_MoisesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Moises_MoisesUITests
-@end
--- Pods/Target Support Files/Pods-Moises/Pods-Moises-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Moises : NSObject
-@end
-@implementation PodsDummy_Pods_Moises
-@end
